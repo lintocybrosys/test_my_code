7dc6fd1b0183d7598197b50cf7027c1187975876
linto <linto@cybrosys.in>
2018-10-23 17:15:38 +0530

    workset phase 4

---
+++ fsm_team_handler/models/skills.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+
+class SkillsHandler(models.Model):
+    _name = 'fsm.skills'
+    _rec_name = 'name'
+    _description = 'Manage skills'
+
+    name = fields.Char(
+        string="Skill Name",
+        required=True
+    )
